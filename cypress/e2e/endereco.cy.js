7707f827efbf6a6bbad253ec040557744e7a7d6d
André Lavorente <lavorente.a@gmail.com>
2023-11-17 13:02:23 -0300

    att

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,17 +1,31 @@
 /// <reference types="cypress" />
+import EnderecoPage from "../support/page-objects/endereco.page";
 
-describe('Funcionalidade Endereços - Faturamento e entrega', () => {
-    beforeEach(() => {
-        cy.visit('minha-conta')
-        cy.fixture('perfil').then(dados => {
-            cy.login(dados.usuario, dados.senha)
-        })
-       
-    })
+describe("Funcionalidade Endereços - Faturamento e entrega", () => {
+  beforeEach(() => {
+    cy.visit("minha-conta");
+    cy.fixture("perfil").then((dados) => {
+      cy.login(dados.usuario, dados.senha);
+    });
+  });
 
-
-
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
-    })
-})
+  it.only("Deve fazer cadastro de faturamento com sucesso", () => {
+    EnderecoPage.editarEnderecoFaturamento(
+      "Andre",
+      "Lavorente",
+      "Google",
+      "Brasil",
+      "Avenida Brasil",
+      "3001",
+      "São Paulo",
+      "São Paulo",
+      "01000100",
+      "1828992332",
+      "email@email.com"
+    );
+    cy.get(".woocommerce-message").should(
+      "contain",
+      "Endereço alterado com sucesso"
+    );
+  });
+});
